9ebe30e60d879a78ee39756d334dbec864045199
sushanth-gs <95746147+sushanth-gs@users.noreply.github.com>
2023-07-10 16:32:30 +0530

    added changes for events number of events and type
    added chenges for events, instance, number of events and type

--- extractXMLParams.py
+++ extractXMLParams.py
@@ -4,7 +4,7 @@ import pandas as pd
 from glob import glob
 from bs4 import BeautifulSoup
 from sys import argv
-#beautiful soup (bs4) to be installed via pip
+#beautiful soup (bs4) and pandas to be installed via pip
 #call from commandline as python extractXMLParams.py <path to xml files> <path to save CSVs>
 def GetAllEventTags(xmlFname):
     """ 
@@ -23,14 +23,18 @@ def GetEventDictList(AllEventTags):
     2.TimeStamp(s)  (set to 00:00:00 as starting, 5:00 is 5*3600.0 seconds+Timefraction(if any)*1e-6) float
     3.Description -- str(description of the event) (E.g. TSP activated)
     4.InfoCategory -- str Category of information (E.g. TSPUsage)
-    5.ImpactsResource -- (bool)does it impact a resource (T/F)  
-    6.Component impacted -- str e.g. IAD, WorkflowArbitration
-    7.What layer --str e.g. Application
-    8.Member -- str Member in the layer e.g. TwoDDistance, AnchorCaliper
-    9.Resource -- Resource impacted
+    5.NumEventsInTag -- (int) Number of actual events in this event tag.
+    6.Events -- Event and type of each event (E.g. <absolute>Event_Set_Caliper</absolute>)
+    7.ImpactsResource -- (bool)does it impact a resource (T/F)  
+    8.Instance -- str (instance impacted(?) not sure)
+    9.Component impacted -- str e.g. IAD, WorkflowArbitration
+    10.What layer --str e.g. Application
+    11.Member -- str Member in the layer e.g. TwoDDistance, AnchorCaliper
+    12.Resource -- Resource impacted
     """
     EventDictList=[]
     for i in range(len(AllEventTags)):
+        print(i)
         EventDict={}
         EventDict['Index']=AllEventTags[i]['Index']
         EventDict['TimeStamp(s)']=float(AllEventTags[i]['TimeStamp'].split(':')[1])*3600.0+float(AllEventTags[i]['TimeStamp'].split(':')[2])*60.0+1e-6*float(AllEventTags[i]['TimeFraction'])
@@ -39,18 +43,50 @@ def GetEventDictList(AllEventTags):
         EventDict['InfoCategory']=AllEventTags[i].EventInfo['InfoCategory']
         childTagList=list(AllEventTags[i].findChildren())
         childTagNames=[t.name for t in childTagList]
+        intVal=AllEventTags[i].PreEventHandlingMemberValue
+
+        if('events' in childTagNames):
+            EventDict['NumEventsInTag']=(len(AllEventTags[i].events)-1)//2
+            EventDict['Events']=[t for t  in AllEventTags[i].events.findChildren()]
+            eventChildTagNames=[t.name for t  in AllEventTags[i].events.findChildren()]
+            for j in range(len(eventChildTagNames)):
+                if(eventChildTagNames[j]!='basic' and eventChildTagNames[j]!='absolute' ):
+                    EventDict['Minimum']=intVal.minimum.decode().split('>')[1].split('<')[0]
+                    EventDict['Maximum']=intVal.maximum.decode().split('>')[1].split('<')[0]
+                else:
+                    EventDict['Minimum']='NA'
+                    EventDict['Maximum']='NA'
+        else:
+            EventDict['NumEventsInTag']=0
+            EventDict['Events']='NA'
+
         if('ResourceMember' in childTagNames):
             EventDict['ImpactsResource']=True 
             EventDict['Component']=AllEventTags[i].ResourceMember.MemberId['Component']
             EventDict['Layer']=AllEventTags[i].ResourceMember.MemberId['Layer']
             EventDict['Member']=AllEventTags[i].ResourceMember.MemberId['Member']
             EventDict['Resource']=AllEventTags[i].ResourceMember.MemberId['Resource']
+            intValChildTags=list(intVal.findChildren())
+            intValChildTagNames=[t.name for t in intValChildTags]
+            if('Instance' in intValChildTagNames):
+                EventDict['Instance']=AllEventTags[i].ResourceMember.MemberId['Instance']
+            else:
+                EventDict['Instance']='NA'
+            EventDict['Available']=bool(intVal.available.decode().split('>')[1].split('<')[0])
+            EventDict['Enabled']=bool(intVal.enabled.decode().split('>')[1].split('<')[0])
+            
+           
         else:
             EventDict['ImpactsResource']=False 
             EventDict['Component']='NA'
             EventDict['Layer']='NA'
             EventDict['Member']='NA'
             EventDict['Resource']='NA'
+            EventDict['Instance']='NA'
+            EventDict['Available']='NA'
+            EventDict['Enabled']='NA'
+            EventDict['Minimum']='NA'
+            EventDict['Maximum']='NA'
         EventDictList.append(EventDict)
     return(EventDictList)
 
